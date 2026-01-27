3eccfadbdd658745c0e177ab167a93b5d7db7832
monirulislamkhan <mr.monirulkhan@gmail.com>
2026-01-27 19:38:04 +0530

    Adding

---
+++ javascript-core.js
@@ -0,0 +1,85 @@
+let x;
+// Old javascript(before ES6 2015)
+// its scope function
+// Can Reassign and Redecleare
+// var a = 10;
+// a = 1;
+// var a = 30;
+// console.log(a);
+
+//  Block scope let
+// can Reassign
+// Can't Redeclear
+
+// let b = 20;
+// b = 30;
+// let b = 35; // can't redeclear
+// console.log(b);
+
+// Const
+// Can't Reassign
+// Can't Redeclear
+// Its Block scope
+// const c = 40;
+// c = 40; // Can't Reassign
+// const c = 50; // Can't Redeclear
+
+// Const not value fixed
+// const reference is fixed
+// const user = { name: 'Monirul' };
+// user.name = "Raju"
+// console.log(user);
+/* var name = 'Dubai housing';
+var age = 10;
+console.log(name);
+var fruit = 'Apple';
+var fruit = 'banana';
+console.log(fruit); */
+/* if (true) {
+  var d = 10;
+}
+console.log(d);
+console.log(car);
+var car = 'Car name'; */
+/* let score = 0;
+score = 10;
+score = 20;
+console.log(score); */
+// Cannot be redeclared
+/* let city = 'Delhi';
+let city = 'Kolkata'; */
+// Block scope
+/* if (true) {
+  let y = 5;
+  console.log(y);
+}
+console.log(y); */
+
+// Create a variable for your age that can change next year
+// Create a variable for your birth year that never changes
+// Write the code here
+
+/* let age = 22;
+age++;
+const birthYear = 2004;
+console.log(age);
+console.log(birthYear); */
+
+/* for (let i = 5; i > 0; i--) {
+  console.log(i);
+}
+console.log(i); */
+
+/* const colors = ['red', 'blue'];
+colors.push('Dubai', 'Ras Al Khaimah');
+colors.unshift('India');
+
+console.log(colors); */
+/* const person = {
+  name: 'Moni',
+  age: 38,
+  city: 'Kolkata',
+};
+person.name = 'Khan';
+
+console.log(person); */
