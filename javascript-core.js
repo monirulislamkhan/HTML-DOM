f9908f2293df30b6e80cab672c144d58ec8db6e8
monirulislamkhan <mr.monirulkhan@gmail.com>
2026-02-02 20:24:15 +0530

    shallow copy and deep copy practices

--- javascript-core.js
+++ javascript-core.js
@@ -1,4 +1,4 @@
-let x;
+// let x;
 // Old javascript(before ES6 2015)
 // its scope function
 // Can Reassign and Redecleare
@@ -83,3 +83,93 @@ console.log(colors); */
 person.name = 'Khan';
 
 console.log(person); */
+/* console.log(a);
+console.log(b);
+console.log(c);
+var a = 10;
+let b = 20;
+const c = 30; */
+
+/* function checkTDZ() {
+  console.log(x);
+  let x = 10;
+  console.log(x);
+}
+checkTDZ(); */
+
+// const tableValue = 3;
+for (var i = 1; i <= 7; i++) {
+  /* setTimeout(function () {
+    console.log(i);
+    // console.log(`${tableValue} X ${i} = ${tableValue * i}`);
+  }, i * 1000); */
+  // IIFE Immediately Invoked function expression
+  /* (function (i) {
+    setTimeout(function () {
+      console.log(i);
+    }, i * 1000);
+  })(i); */
+}
+/* let infinity = -Infinity;
+console.log(infinity, typeof infinity); */
+
+/* const arr1 = [1,2,3]
+const arr2 = arr1;
+arr1.push(4)
+
+
+console.log(arr1);
+console.log(arr2); */
+
+/* function changeArray(arr) {
+  arr.push(999);
+}
+let numbers = [1, 2, 3];
+changeArray(numbers);
+console.log(numbers); */
+
+/* let original = [1, 2, 3, 4];
+let copy = [...original];
+copy.push(5);
+console.log(original);
+console.log(copy); */
+/* let person1 = { name: 'Monirul', age: 38 };
+let person2 = { ...person1 };
+person2.age = 29;
+person2.city = 'Kolkata';
+
+console.log(person1);
+console.log(person2); */
+
+/* let original = {
+  name: 'Monirul',
+  age: 38,
+  address: {
+    city: 'Kolkata',
+  },
+};
+let copy = JSON.parse(JSON.stringify(original));
+copy.address.city = 'Dubai';
+console.log(original);
+console.log(copy); */
+
+/* let obj1 = { value: 10 };
+let obj2 = obj1;
+obj1.value = 20;
+let obj3 = obj1;
+console.log(obj1);
+console.log(obj2);
+console.log(obj3); */
+
+// console.log(arr1 === arr2);
+
+/* let a = { x: 10 };
+let b = a;
+let c = { x: 10 };
+console.log(a === b);
+console.log(a === c); */
+let original = [1, 2, 3];
+let copy = [...original];
+copy.push(4);
+console.log(copy);
+console.log(original);
